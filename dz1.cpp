9b78d62c14ff3de2f3be8a768316999b39a43f7c
Elizaveta Makarenko <lizamak04@gmail.com>
2023-11-13 00:17:11 +0300

    add input

--- dz1.cpp
+++ dz1.cpp
@@ -4,21 +4,64 @@
 #include <string>
 #include <algorithm>
 #include <ranges> 
+#include <fstream> 
 
 #include "sorter.h" 
 #include "quickSort.h"
 
 using namespace std;
 
+string readFile(const string& fileName)
+{
+    ifstream file(fileName);
+    if (!file.is_open())
+    {
+        throw runtime_error("Could not open file " + fileName);
+    }
+
+    stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+vector<string> split(const string& s) {
+    vector<string> words;
+    string word;
+    for (char c : s) {
+        if (c == ' ' || c == '\n') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
+            }
+        }
+        else {
+            word += c;
+        }
+    }
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+    return words;
+}
+
+void printStrings(const vector<string>& strings) {
+    for (const auto& str : strings) {
+        cout << str << endl;
+    }
+}
 
 int main()
 {   
-    vector<string> arr{ "cc", "ab", "aa" };
+    string fullStr = readFile("in.txt");
+    cout << fullStr << endl << endl;
+
+    vector<string> words = split(fullStr);
 
     Sorter* sorter =  new QuickSort();
     
-    cout << arr[0] << " " << arr[1] << " " << arr[2] << endl;
-    sorter->Sort(arr);
-    cout << arr[0] << " " << arr[1] << " " << arr[2] << endl;
+    sorter->Sort(words);
+
+    printStrings(words); cout << endl;
+    
 }
 
