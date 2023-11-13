487c8d7c6b70cf920db61b4b32a468a1bf8161c8
Elizaveta Makarenko <lizamak04@gmail.com>
2023-11-13 23:08:53 +0300

    add validate

--- dz1.cpp
+++ dz1.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <ranges> 
 #include <fstream> 
+#include <regex> 
 
 #include "sorter.h" 
 #include "quickSort.h"
@@ -22,6 +23,11 @@ string readFile(const string& fileName)
     stringstream buffer;
     buffer << file.rdbuf();
     return buffer.str();
+}
+
+bool validateString(const string& str) {
+    regex reg("^[a-zA-Z \n]*$"); 
+    return regex_match(str, reg);
 }
 
 vector<string> split(const string& s) {
@@ -60,14 +66,22 @@ int main()
     };
 
     while (true) {
+        cout << "filename: ";
         string fileName;
         cin >> fileName;
         if (fileName == "q") {
             break;
         }
+        string fullFileName = "input/" + fileName;
 
-        string fullStr = readFile(fileName);
+
+        string fullStr = readFile(fullFileName);
         cout << fullStr << endl << endl;
+
+        if (!validateString(fullStr)) {
+            cout << "file " << fileName << " is invalid" << endl;
+            continue;
+        }
 
         vector<string> words = split(fullStr);
 
@@ -76,7 +90,7 @@ int main()
         for (Sorter* sorter : sorters) {
             sorter->Sort(words);
 
-            cout << sorter->sortedName << endl << stringsToLine(words) << endl;
+            cout << sorter->sortedName << endl << stringsToLine(words) << endl << endl;
         }
     }    
 }
